ac13c284b57148111f5c8b996d3febaf5099cfbb
Mahmoud Osman <Mahmoud4501@gmail.com>
2025-11-21 17:26:45 +0200

    Messaging update

--- Common.Application/Abstractions/Messaging/IQuery.cs
+++ Common.Application/Abstractions/Messaging/IQuery.cs
@@ -3,6 +3,9 @@ using MediatR;
 
 namespace Common.Application.Abstractions.Messaging;
 
+public interface IQuery: IRequest<ResponseModel>
+{
+}
 public interface IQuery<TResponse> : IRequest<ResponseModel<TResponse>>
 {
 }
--- Common.Application/Abstractions/Messaging/IQueryHandler.cs
+++ Common.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -3,6 +3,10 @@ using MediatR;
 
 namespace Common.Application.Abstractions.Messaging;
 
+public interface IQueryHandler<TQuery> : IRequestHandler<TQuery, ResponseModel>
+    where TQuery : IQuery
+{
+}
 public interface IQueryHandler<TQuery, TResponse>
     : IRequestHandler<TQuery, ResponseModel<TResponse>>
     where TQuery : IQuery<TResponse>
